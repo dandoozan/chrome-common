c65124b0d49b1808a7dd74baaa4c592f1b293d66
Dan Doozan <dandoozan@gmail.com>
2018-11-05 23:26:08 -0700

    Background: Add some functions, and convert all functions to use Promises

--- background.js
+++ background.js
@@ -1,47 +1,116 @@
-
 export function listenForReload() {
     require('./_listenForReload');
 }
 
-export function getCurrentTab(cb) {
-    chrome.tabs.query({
-        currentWindow: true,
-        active: true
-    }, function (tabs) {
-        cb(tabs[0]);
+export function getCurrentWindow() {
+    return new Promise((resolve, reject) => {
+        chrome.windows.getCurrent(null, resolve);
     });
 }
-export function getAllTabs(cb) {
-    chrome.tabs.query({
-        currentWindow: true
-    }, (tabs) => {
-        cb(tabs);
+export function setFullscreenOff(window) {
+    return new Promise((resolve, reject) => {
+        chrome.windows.update(window.id, { state: 'normal'}, resolve);
     });
 }
-export function getAllSelectedTabs(cb) {
-    chrome.tabs.query({
-        currentWindow: true,
-        highlighted: true
-    }, (tabs) => {
-        cb(tabs);
+export function getCurrentTab() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            resolve(tabs[0]);
+        });
     });
 }
-export function closeTab(tab, cb) {
-    chrome.tabs.remove(tab.id, cb);
+export function getAllTabs() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ currentWindow: true }, (tabs) => {
+            resolve(tabs);
+        });
+    });
 }
-export function closeTabs(tabs, cb) {
-    chrome.tabs.remove(tabs.map((tab) => tab.id), cb);
+export function getAllSelectedTabs() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ currentWindow: true, highlighted: true }, (tabs) => {
+            resolve(tabs);
+        });
+    });
+}
+export function closeTab(tab) {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.remove(tab.id, resolve);
+    });
+}
+export function closeTabs(tabs) {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.remove(tabs.map((tab) => tab.id), resolve);
+    })
 }
 
-export function createNewWindow(cb) {
-    chrome.windows.create({}, cb);
+export function createNewWindow(windowOptions) {
+    return new Promise((resolve, reject) => {
+        chrome.windows.create(windowOptions, (window) => {
+            resolve(window);
+        });
+    });
 }
 
-export function moveTabToNewWindow(tab, cb) {
-    chrome.windows.create({ tabId: tab.id }, cb);
+export async function moveTabToNewWindow(tab) {
+    return await createNewWindow({ tabId: tab.id });
 }
 
-export function moveTabsToExistingWindow(tabs, windowId, cb) {
-    const tabIds = tabs.map((tab) => tab.id);
-    chrome.tabs.move(tabIds, { windowId, index: -1 }, cb);
+export function moveTabsToExistingWindow(tabs, windowId) {
+    return new Promise((resolve, reject) => {
+        const tabIds = tabs.map((tab) => tab.id);
+        chrome.tabs.move(tabIds, { windowId, index: -1 }, (tabs) => {
+            resolve(tabs);
+        });
+    });
 }
+
+function getDisplayInfo() {
+    return new Promise((resolve, reject) => {
+        chrome.system.display.getInfo((displays) => {
+            //displays is an array of displays, which presumably is for when a user
+            //has multiple monitors, but i dont so I'm just taking the first one
+            resolve(displays[0]);
+        });
+    });
+}
+
+export async function getScreenWidth() {
+    const displayInfo = await getDisplayInfo();
+
+    //use workArea since this is the area that excludes the menu bar at the
+    //of the screen
+    return displayInfo.workArea.width;
+}
+export async function getScreenHeight() {
+    const displayInfo = await getDisplayInfo();
+
+    //use workArea since this is the area that excludes the menu bar at the
+    //of the screen
+    return displayInfo.workArea.height;
+}
+
+export async function moveWindowToRightSide(window) {
+    return new Promise(async (resolve, reject) => {
+        const options = {
+            left: await getScreenWidth() / 2,
+            top: 0,
+            width: await getScreenWidth() / 2,
+            height: await getScreenHeight(),
+        };
+        chrome.windows.update(window.id, options, resolve);
+    });
+}
+export async function moveWindowToLeftSide(window) {
+    return new Promise(async (resolve, reject) => {
+        const options = {
+            left: 0,
+            top: 0,
+            width: await getScreenWidth() / 2,
+            height: await getScreenHeight(),
+        };
+        chrome.windows.update(window.id, options, resolve);
+    });
+}
+
+
