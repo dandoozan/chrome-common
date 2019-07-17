234831de6f027c0026091e55941ac233351e38f0
Dan Doozan <dandoozan@gmail.com>
2019-07-16 22:28:08 -0700

    Fix bug in injectCustomContentScripts

--- injectCustomContentScripts.js
+++ injectCustomContentScripts.js
@@ -5,10 +5,19 @@ import { loadOptions, getContentScriptObject } from '../utils';
 
     //todo: make sure i'm not injecting scripts twice (which i think will happen when a tab updates
     //in multiple ways (eg. title changes, moved, etc))
-    chrome.tabs.onUpdated.addListener(async function(tabId, { status }, { url }) {
+    chrome.tabs.onUpdated.addListener(async function(
+        tabId,
+        { status },
+        { url }
+    ) {
         if (status === 'complete') {
             //get the content scripts to inject for this url
-            let contentScriptObj = await getContentScriptObject(url, contentScripts);
+            //NOTE: make sure an array is passed in for contentScripts to force it to search
+            //there (and not go looking for the contentScriptObj elsewhere (ie. in manifest.json)).
+            let contentScriptObj = await getContentScriptObject(
+                url,
+                contentScripts || []
+            );
             if (contentScriptObj && contentScriptObj.js) {
                 //inject the scripts in "js"
                 contentScriptObj.js.forEach(file => {
