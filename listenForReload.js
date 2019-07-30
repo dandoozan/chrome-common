2cfc464bbc87bb769634adce67f1a6c1f348af55
Dan Doozan <dandoozan@gmail.com>
2019-07-30 12:29:01 -0700

    Prettierify listenForReload.js

--- listenForReload.js
+++ listenForReload.js
@@ -6,18 +6,24 @@ const BADGE_COLOR = '#4cb749'; //green
 const BADGE_TIMEOUT_IN_MS = 1000;
 
 function showBadge(msgOf4CharsOrLess, colorAsCSSCompatibleString) {
-    chrome.browserAction.setBadgeText({text: msgOf4CharsOrLess});
-    chrome.browserAction.setBadgeBackgroundColor({color: colorAsCSSCompatibleString});
+    chrome.browserAction.setBadgeText({ text: msgOf4CharsOrLess });
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: colorAsCSSCompatibleString,
+    });
 }
 
 function removeBadge() {
     //i guess just set the badge text to '' to remove it; i don't see anything in
     //the documentation for how to remove a badge, and I can't think of any other way
     //to do it
-    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setBadgeText({ text: '' });
 }
 
-function flashBadge(msgOf4CharsOrLess, colorAsCSSCompatibleString, timeoutInMs) {
+function flashBadge(
+    msgOf4CharsOrLess,
+    colorAsCSSCompatibleString,
+    timeoutInMs
+) {
     showBadge(msgOf4CharsOrLess, colorAsCSSCompatibleString);
     setTimeout(removeBadge, timeoutInMs);
 }
@@ -31,9 +37,10 @@ function reloadThisExtension() {
 }
 
 function handleOnMessageExternal(request, sender, sendResponse) {
-    if (sender.id === CRX_RELOADER_EXTENSION_ID) { //ensure the msg is coming from the CrxReloader extension
+    if (sender.id === CRX_RELOADER_EXTENSION_ID) {
+        //ensure the msg is coming from the CrxReloader extension
         if (request.msg === RELOAD_MESSAGE) {
-            reloadThisExtension()
+            reloadThisExtension();
         }
     }
 }
