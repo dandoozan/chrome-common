73bd3dea5cd7a1b2856674cdc960aafdf688c7b1
Dan Doozan <dandoozan@gmail.com>
2019-10-04 22:19:38 -0700

    Add "onMessage" and "sendMessageToCurrentTab"

--- src/utils.js
+++ src/utils.js
@@ -1,159 +1,177 @@
 export function getManifest() {
-    return chrome.runtime.getManifest();
+  return chrome.runtime.getManifest();
 }
 
 export async function loadOptions() {
-    //first try to get options from options.json
-    try {
-        return require('../../options.json');
-    } catch (error) {
-        //otherwise, get the options from storage
-        return await readFromStorage({ options: {} });
-    }
+  //first try to get options from options.json
+  try {
+    return require('../../options.json');
+  } catch (error) {
+    //otherwise, get the options from storage
+    return await readFromStorage({ options: {} });
+  }
 }
 
 export function readFromStorage(whatToGet) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(whatToGet, resolve);
-    });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(whatToGet, resolve);
+  });
 }
 
 export function clearStorage() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.clear(resolve);
-    });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.clear(resolve);
+  });
 }
 
 export function writeToStorage(whatToSet) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.set(whatToSet, resolve);
-    });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.set(whatToSet, resolve);
+  });
+}
+
+export function onMessage(msg, callback) {
+  chrome.runtime.onMessage.addListener(function(
+    { message },
+    sender,
+    sendResponse
+  ) {
+    if (message === msg) {
+      sendResponse(callback());
+    } else {
+      sendResponse(null);
+    }
+
+    return true; //<-- return true to let the message sender know that I'm definitely going to be calling sendResponse (otherwise, it will timeout)
+  });
+}
+
+export async function sendMessageToCurrentTab(message) {
+  return new Promise(async function(resolve) {
+    let { id } = await getCurrentTab();
+    chrome.tabs.sendMessage(id, { message }, resolve);
+  });
 }
 
 export async function getContentScriptObject(url, contentScripts) {
-    //this function gets the "matches" object for the given url (ie. the
-    //object that looks like:
-    //{
-    //     "matches": ["https://www.google.com/*"],
-    //     "exclude_matches": ["https://www.google.com/search?*"],
-    //     "js": [
-    //         "js/contentScripts/google.bundle.js",
-    //         "js/contenScripts/_main.js"
-    //     ],
-    //     "my_custom_property": "value"
-    //}
+  //this function gets the "matches" object for the given url (ie. the
+  //object that looks like:
+  //{
+  //     "matches": ["https://www.google.com/*"],
+  //     "exclude_matches": ["https://www.google.com/search?*"],
+  //     "js": [
+  //         "js/contentScripts/google.bundle.js",
+  //         "js/contenScripts/_main.js"
+  //     ],
+  //     "my_custom_property": "value"
+  //}
 
-    function isMatch(url, urlGlobs) {
-        if (url && urlGlobs) {
-            for (let urlGlob of urlGlobs) {
-                if (
-                    url === urlGlob ||
-                    convertMatchPatternToRegExp(urlGlob).test(url)
-                ) {
-                    return true;
-                }
-            }
+  function isMatch(url, urlGlobs) {
+    if (url && urlGlobs) {
+      for (let urlGlob of urlGlobs) {
+        if (url === urlGlob || convertMatchPatternToRegExp(urlGlob).test(url)) {
+          return true;
         }
-        return false;
+      }
+    }
+    return false;
+  }
+
+  //if contenScripts has been passed in, get it from there
+  if (contentScripts) {
+    for (let contentScriptObj of contentScripts) {
+      let { matches, exclude_matches } = contentScriptObj;
+      //if the url matches one of the urls in "matches" and
+      //does NOT match one of the urls in "exclude_matches", return this obj
+      if (isMatch(url, matches) && !isMatch(url, exclude_matches)) {
+        return contentScriptObj;
+      }
+    }
+  } else {
+    //otherwise, first try to get it from manifest.json
+    let contentScriptObj = await getContentScriptObject(
+      url,
+      getManifest().content_scripts || []
+    );
+    if (contentScriptObj) {
+      return contentScriptObj;
     }
 
-    //if contenScripts has been passed in, get it from there
-    if (contentScripts) {
-        for (let contentScriptObj of contentScripts) {
-            let { matches, exclude_matches } = contentScriptObj;
-            //if the url matches one of the urls in "matches" and
-            //does NOT match one of the urls in "exclude_matches", return this obj
-            if (isMatch(url, matches) && !isMatch(url, exclude_matches)) {
-                return contentScriptObj;
-            }
-        }
-    } else {
-        //otherwise, first try to get it from manifest.json
-        let contentScriptObj = await getContentScriptObject(
-            url,
-            getManifest().content_scripts || []
-        );
-        if (contentScriptObj) {
-            return contentScriptObj;
-        }
-
-        //if that doesn't work, then try to get it from options
-        return await getContentScriptObject(
-            url,
-            (await loadOptions()).content_scripts || []
-        );
-    }
+    //if that doesn't work, then try to get it from options
+    return await getContentScriptObject(
+      url,
+      (await loadOptions()).content_scripts || []
+    );
+  }
 }
 
 export function getExtensionId() {
-    return chrome.runtime.id;
+  return chrome.runtime.id;
 }
 
 export function isWindowFullscreen(window) {
-    return window.state === 'fullscreen';
+  return window.state === 'fullscreen';
 }
 
 export function getCurrentWindow() {
-    return new Promise((resolve, reject) => {
-        chrome.windows.getCurrent(null, resolve);
-    });
+  return new Promise((resolve, reject) => {
+    chrome.windows.getCurrent(null, resolve);
+  });
 }
 
 export function getAllWindows() {
-    return new Promise((resolve, reject) => {
-        chrome.windows.getAll(
-            {
-                populate: true,
-                windowTypes: ['normal'],
-            },
-            resolve
-        );
-    });
+  return new Promise((resolve, reject) => {
+    chrome.windows.getAll(
+      {
+        populate: true,
+        windowTypes: ['normal'],
+      },
+      resolve
+    );
+  });
 }
 
 export function getCurrentTab() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({ currentWindow: true, active: true }, function(
-            tabs
-        ) {
-            resolve(tabs[0]);
-        });
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+      resolve(tabs[0]);
     });
+  });
 }
 export function getAllTabs() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({ currentWindow: true }, tabs => {
-            resolve(tabs);
-        });
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ currentWindow: true }, tabs => {
+      resolve(tabs);
     });
+  });
 }
 export function getAllSelectedTabs() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({ currentWindow: true, highlighted: true }, tabs => {
-            resolve(tabs);
-        });
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ currentWindow: true, highlighted: true }, tabs => {
+      resolve(tabs);
     });
+  });
 }
 
 function getDisplayInfo() {
-    return new Promise((resolve, reject) => {
-        chrome.system.display.getInfo(displays => {
-            resolve(displays[0]);
-        });
+  return new Promise((resolve, reject) => {
+    chrome.system.display.getInfo(displays => {
+      resolve(displays[0]);
     });
+  });
 }
 
 export async function getScreenWidth() {
-    const displayInfo = await getDisplayInfo();
+  const displayInfo = await getDisplayInfo();
 
-    //use workArea since that excludes the menu bar at the top of the screen
-    return displayInfo.workArea.width;
+  //use workArea since that excludes the menu bar at the top of the screen
+  return displayInfo.workArea.width;
 }
 export async function getScreenHeight() {
-    const displayInfo = await getDisplayInfo();
+  const displayInfo = await getDisplayInfo();
 
-    //use workArea since that excludes the menu bar at the top of the screen
-    return displayInfo.workArea.height;
+  //use workArea since that excludes the menu bar at the top of the screen
+  return displayInfo.workArea.height;
 }
 
 /*The below is from: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Match_patterns
@@ -166,60 +184,60 @@ export async function getScreenHeight() {
  * @throws {TypeError}        If the pattern is not a valid MatchPattern
  */
 function convertMatchPatternToRegExp(pattern) {
-    if (pattern === '') {
-        return /^(?:http|https|file|ftp|app):\/\//;
+  if (pattern === '') {
+    return /^(?:http|https|file|ftp|app):\/\//;
+  }
+
+  const schemeSegment = '(\\*|http|https|file|ftp)';
+  const hostSegment = '(\\*|(?:\\*\\.)?(?:[^/*]+))?';
+  const pathSegment = '(.*)';
+  const matchPatternRegExp = new RegExp(
+    `^${schemeSegment}://${hostSegment}/${pathSegment}$`
+  );
+
+  let match = matchPatternRegExp.exec(pattern);
+  if (!match) {
+    throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
+  }
+
+  let [, scheme, host, path] = match;
+  if (!host) {
+    throw new TypeError(`"${pattern}" does not have a valid host`);
+  }
+
+  let regex = '^';
+
+  if (scheme === '*') {
+    regex += '(http|https)';
+  } else {
+    regex += scheme;
+  }
+
+  regex += '://';
+
+  if (host && host === '*') {
+    regex += '[^/]+?';
+  } else if (host) {
+    if (host.match(/^\*\./)) {
+      regex += '[^/]*?';
+      host = host.substring(2);
     }
+    regex += host.replace(/\./g, '\\.');
+  }
 
-    const schemeSegment = '(\\*|http|https|file|ftp)';
-    const hostSegment = '(\\*|(?:\\*\\.)?(?:[^/*]+))?';
-    const pathSegment = '(.*)';
-    const matchPatternRegExp = new RegExp(
-        `^${schemeSegment}://${hostSegment}/${pathSegment}$`
-    );
-
-    let match = matchPatternRegExp.exec(pattern);
-    if (!match) {
-        throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
+  if (path) {
+    if (path === '*') {
+      regex += '(/.*)?';
+    } else if (path.charAt(0) !== '/') {
+      regex += '/';
+      regex += path
+        .replace(/\./g, '\\.')
+        .replace(/\?/g, '\\?')
+        .replace(/\*/g, '.*?');
+      regex += '/?';
     }
+  }
 
-    let [, scheme, host, path] = match;
-    if (!host) {
-        throw new TypeError(`"${pattern}" does not have a valid host`);
-    }
-
-    let regex = '^';
-
-    if (scheme === '*') {
-        regex += '(http|https)';
-    } else {
-        regex += scheme;
-    }
-
-    regex += '://';
-
-    if (host && host === '*') {
-        regex += '[^/]+?';
-    } else if (host) {
-        if (host.match(/^\*\./)) {
-            regex += '[^/]*?';
-            host = host.substring(2);
-        }
-        regex += host.replace(/\./g, '\\.');
-    }
-
-    if (path) {
-        if (path === '*') {
-            regex += '(/.*)?';
-        } else if (path.charAt(0) !== '/') {
-            regex += '/';
-            regex += path
-                .replace(/\./g, '\\.')
-                .replace(/\?/g, '\\?')
-                .replace(/\*/g, '.*?');
-            regex += '/?';
-        }
-    }
-
-    regex += '$';
-    return new RegExp(regex);
+  regex += '$';
+  return new RegExp(regex);
 }
